7a6402a423ac04d8c1f19b175871b7f2b099e923
Ogo <36090819+oguzhanyazman@users.noreply.github.com>
2023-07-25 03:30:59 +0300

    Create sunucuKodu.py

---
+++ sunucuKodu.py
@@ -0,0 +1,19 @@
+import pika
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+
+queue_name = 'ogox'
+
+
+channel.queue_declare(queue=queue_name)
+
+
+def send_message(message):
+    channel.basic_publish(exchange='', routing_key=queue_name, body=message)
+    print(f" [x] Sunucu: Mesaj gönderildi: {message}")
+
+
+send_message("Merhaba mikrocan!")
